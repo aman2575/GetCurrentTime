b5fcb1127d59ceb4135b23f2304d1503002bcd78
aman2575 <git-autoconfig>
2021-10-23 16:28:18 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  let newTime = new Date().toLocaleTimeString();
+  const [ctime, setCtime] = useState(newTime);
+  const UpdateTime = () => {
+    let newCtime = new Date().toLocaleTimeString();
+    setCtime(newCtime)
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1> {ctime} </h1>
+      <button onClick={UpdateTime}> get time</button>
+    </>
+  )
 }
 
-export default App;
+export default App
